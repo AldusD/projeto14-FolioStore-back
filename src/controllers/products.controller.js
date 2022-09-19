17e6b42fa69223339a138d88ee4ee329d450cc26
pauloserrano <pvsserrano@gmail.com>
2022-09-19 12:45:55 -0300

    getProducts with filtering

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -2,23 +2,26 @@ import db from "../db/db.js";
 import COLLECTIONS from '../enums/collections.js';
 import STATUS from '../enums/status.js';
 
-// PLACEHOLDER
-// { title, from, price, description, img, category }
+
 const getProducts = async (req, res) => {
     try {
         const categories = await db.collection(COLLECTIONS.CATEGORIES).find().toArray()
-        const products = categories.map(({ category }) => ({
-            category,
-            products: []
-        }))
+        const products = await db.collection(COLLECTIONS.PRODUCTS).find().toArray()
 
-        res.send(products);
+        const filteredProducts = categories.map(({_id, category}) => ({
+                category,
+                products: products.filter((product) => product.category === _id.toString())
+            })
+        )
+
+        res.send(filteredProducts);
 
     } catch (error) {
         res.status(STATUS.SERVER_ERROR).send(error);
     }
 }
 
+// DEV
 const setProduct = async (req, res) => {
     try {
         db.collection(COLLECTIONS.PRODUCTS).insertOne(req.body)
