6b95d2a399e5ada667abe82ce236bb8339f64fa4
aldusd <aldusdanielfsm@gmail.com>
2022-09-19 15:06:16 -0300

    fixing

--- src/routes/checkout.route.js
+++ src/routes/checkout.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 
 import * as controller from '../controllers/checkout.controller.js';
+import * as middleware from '../middleware/checkout.middleware.js';
 
 const router = express.Router();
 
-router.get("/checkout/:USER_ID", controller.checkout);
+router.post("/checkout/:USER_ID", middleware.verifyUser, controller.checkout);
 
 export default router;
